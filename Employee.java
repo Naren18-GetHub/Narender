2925712896ac4296fe167f3acc7096532bae9dd7
Narender Yadav <narenderyadav1810@gmail.com>
2024-08-20 15:22:17 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+package Lab_1st;
+
+public class Employee {
+    protected int ID;
+    protected int age = (int)35.5;   //Assign the value of age
+    protected String name;
+    protected boolean isPermanent;
+
+
+    public static void main(String[] args) {
+        System.out.println("Successfully Started");
+    }
+}
---
+++ Student.java
@@ -0,0 +1,13 @@
+package Lab_1st;
+
+public class Student {
+    // Instance variables (attributes)
+    private String studentName;
+    private String collegeName;
+
+    private int studentID;
+
+    public static void main(String[] args){
+        System.out.println("Successful");
+    }
+}
